9513cd287b78d95d3e973a09ba5ca3d41b877e33
Igor Medovolkin <igor.medovolkin@jetbrains.com>
2016-05-14 04:04:40 +0300

    relative path to config file in init script

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ mkdir -p ~/web/{etc,uploads,public}/
 mkdir -p ~/web/public/{img,js,css}/
 
 echo moving and linking nginx configuration file...
-mv ./nginx.conf ~/web/etc/nginx.conf
+mv $(dirname $0)/nginx.conf ~/web/etc/nginx.conf
 sudo ln -sf ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 
 echo restarting nginx...
