f0f65b3f61d4066fb18ec6294128db0841fd03c4
Igor Medovolkin <igor.medovolkin@jetbrains.com>
2016-05-19 03:26:47 +0300

    environment specific changes

--- init.sh
+++ init.sh
@@ -12,6 +12,10 @@ sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf ~/web/etc/hello.conf /etc/gunicorn.d/hello
 sudo ln -sf ~/web/etc/ask.conf /etc/gunicorn.d/ask
 
+echo updating django...
+sudo pip uninstall django
+sudo pip3 install django
+
 echo restarting daemons...
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
