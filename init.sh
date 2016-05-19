2d2720111db9c1ff29bf259812fdf96df9f03482
Igor Medovolkin <igor.medovolkin@jetbrains.com>
2016-05-19 03:43:48 +0300

    small fixes

--- init.sh
+++ init.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 echo copying application files...
-cp -fR $(dirname $0)/web/ ~/web/
+cp -TR $(dirname $0)/web/ ~/web/
 
 echo creating project structure...
 mkdir -p ~/web/{uploads,public}/
 mkdir -p ~/web/public/{img,js,css}/
 
 echo linking configuration files...
+
+
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf ~/web/etc/hello.conf /etc/gunicorn.d/hello
 sudo ln -sf ~/web/etc/ask.conf /etc/gunicorn.d/ask
--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -6,10 +6,10 @@ from qa.views import test
 urlpatterns = [
     url(r'^$', test, name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', test, name='login'),
-    url(r'^signup/$', test, name='signup'),
-    url(r'^question/(?P<id>\d+)/$', test, name='question'),
-    url(r'^ask/$', test, name='ask'),
-    url(r'^popular/$', test, name='popular'),
-    url(r'^new/$', test, name='new')
+    url(r'^login/', test, name='login'),
+    url(r'^signup/', test, name='signup'),
+    url(r'^question/(?P<id>\d+)/', test, name='question'),
+    url(r'^ask/', test, name='ask'),
+    url(r'^popular/', test, name='popular'),
+    url(r'^new/', test, name='new')
 ]
